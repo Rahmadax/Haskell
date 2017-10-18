ba2b59b3eb015b855487be701884c65878c24cfa
Ollie <ollierahman@gmail.com>
2017-10-18 03:13:20 +0100

    All done bar scoreN

--- Haskell/assignment1.hs
+++ Haskell/assignment1.hs
@@ -1,72 +1,60 @@
--------------------------------------------------------
+--------------------------------------------------------------------------
 -- Assignment 1 - Dominoes (Part 1)
 -- Written by Oliver Rahman
 -- Written and fully tested functions go in this file.
--------------------------------------------------------
+--------------------------------------------------------------------------
 module Assignment1 where 
-  ----------------------------
-  -- Creating Datatypes
-  ----------------------------
+  ------------------------------------------------------------------------
+  -- Datatypes
   -- Each domino has a left and right integer value
   type Domino = (Int, Int)
   -- Hand contains a list of dominoes
   type Hand = [Domino]
-  -- Set contains a list of all dominoes (For later assignments)
   type Set = [Domino]
   -- Board contains a list of dominoes in the order they are lined up
   type Board = [Domino]
   -- Either the left or right most domino. 
   data End = L | R deriving (Eq)
   
-  ----------------------
-  -- Init
-  ----------------------
+  ------------------------------------------------------------------------
+  -- Initialisation
+  set :: Set 
+  set = [(a, b)| a <- [0..pips], b <- [a..pips]]
   -- The maximum number of pips on each side of the domino
   pips :: Int
   pips = 6
-  -- Inits the complete set of dominoes
-  set :: Set
-  set  = [(a, b)| a <- [0..pips], b <- [a..pips]]
- 
- 
-  ---------------------------------------
+
+  ------------------------------------------------------------------------
   -- swapDom function
   -- Swaps the order of a Domino tuple
-  ---------------------------------------
   -- type definition
   swapDom :: (a,b) -> (b,a)
   -- function definition
   swapDom (a,b) = (b,a)
   
-  
-  --------------------------------------------------
+  ------------------------------------------------------------------------
   -- goesP function 
   -- True if a domino can be played at a given end.
-  --------------------------------------------------
   -- type definition
   goesP :: Board -> Domino -> End -> Bool
   -- function definition
   goesP [] _ _ = True
-  goesP board (a,b) L = (fd == a) || (fd == b)
-    where fd = fst (head board) -- first domino on the board (left pips)
-  goesP board (a,b) R = (ld == a) || (ld == b)
-    where ld = snd (last board) -- last domino on the board (right pips)
+  goesP board (a,b) L = (leftPips == a) || (leftPips == b)
+    where leftPips = fst (head board) -- first domino's left pips
+  goesP board (a,b) R = (rightPips == a) || (rightPips == b)
+    where rightPips = snd (last board) -- last domino's right pips
 
-
-  ------------------------------------
+  ------------------------------------------------------------------------
   -- playedP function 
   -- True if a domino has been played
-  ------------------------------------
   -- type definition
   playedP :: Board -> Domino -> Bool
   -- function definition
   playedP board domino = elem domino board
-
   
-  -----------------------------------
+  ------------------------------------------------------------------------
   -- knockingP function
   -- True if no domino can be played
-  -----------------------------------
   -- type definition
   knockingP :: Board -> Hand -> Bool
   -- function definition
@@ -77,12 +65,10 @@ module Assignment1 where
     | otherwise = False
     where checkSide = goesP board h 
  
- 
   -------------------------------------------------------------------------   
   -- playDom function and Aux placeDom
   -- Returns the board after a given domino has been played at a given end 
   -- Returns nothing if impossible
-  ------------------------------------------------------------------------- 
   -- type definition
   playDom :: Board -> Domino -> End -> Maybe Board
   -- function definition
@@ -104,41 +90,72 @@ module Assignment1 where
     |(snd lb == snd domino) = board ++ [swapDom domino]
     where lb = (last board)
  
- 
-  ---------------------------------------------------------
+  ------------------------------------------------------------------------
   -- scoreBoard function and Aux getTotal and getScore
   -- Takes a board, returns an integer - the board's score
-  ---------------------------------------------------------
   -- type definition  
   scoreBoard :: Board -> Int
   -- function definition
   scoreBoard [] = 0
-  scoreBoard (h:t)
-    | (((total `mod` 3) == 0 ) || ((total `mod` 5) == 0 )) = getScore total scores
+  scoreBoard board
+    | ((total `mod` 3) == 0 || (total `mod` 5) == 0) = getScore total scores
     | otherwise = 0
    where 
-   scores = [(0,0),(3,1),(5,1),(6,2),(9,3),(10,2),(12,4),(15,8),(18,6),(20,4)]
-   total = getTotal (h:t)
+   scores = [(3,1),(5,1),(6,2),(9,3),(10,2),(12,4),(15,8),(18,6),(20,4)]
+   total = getTotal board
 
-  -- scoreBoard takes a board, returns the sum of its ends.
   -- type definition
   getTotal :: Board -> Int
   -- function definition
-  getTotal (h:t)
-    | null t                                   = fst h + snd h            -- only 1 domino on the board
-    | ((fst h == snd h) && (fst lb == snd lb)) = ((fst h) + (snd lb))*2   -- a pair on each end
-    | (fst h == snd h)                         = (fst h)*2 + (snd lb)     -- a pair on the left end
-    | (fst lb == snd lb)                       = (fst h) + (snd lb)*2     -- a pair on the right end
-    | otherwise                                = fst h + snd lb           -- no pairs
-    where lb = last t
-
-  -- getScore, takes the score table and the total score of the board's ends
-  -- returns the actual score
+  getTotal (hb:tb)
+    -- only 1 domino on the board
+    | null tb = fst hb + snd hb        
+    -- a pair on each end	
+    | ((fst hb == snd hb) && (fst lb == snd lb)) = ((fst hb)+(snd lb))*2
+    -- a pair on the left end
+    | (fst hb == snd hb) = (fst hb)*2 + (snd lb)    
+    -- a pair on the right end	
+    | (fst lb == snd lb) = (fst hb) + (snd lb)*2 
+    -- no pairs	
+    | otherwise = fst hb + snd lb           
+    where lb = last tb
+ 
   -- type definition
   getScore :: Int -> [(Int, Int)] -> Int
   --function definition
   getScore _ [] = 0
-  getScore total (h:t) 
-    | (fst h == total) = snd h
-    | otherwise = getScore total t 
-	
+  getScore total (hs:ts) 
+    | (fst hs == total) = snd hs
+    | otherwise = getScore total ts
+
+  ------------------------------------------------------------------------
+  -- possPlays function
+  -- Takes a Board and Hand, returns all scoring dominoes
+  -- type definition
+  possPlays :: Board -> Hand -> ([Domino], [Domino])
+  -- function definition
+  possPlays [] hand = (hand, hand)
+  possPlays board hand = 
+    (filter (\domino -> goesP board domino L) hand, -- Left end
+    filter (\domino -> goesP board domino R) hand)  -- Right end
+
+  ------------------------------------------------------------------------
+  -- scoreN function
+  -- Takes a Board and an integer n, returns all dominoes that score n
+  
+  scoreN :: Board -> Int -> ([Domino],[Domino])
+  
+  scoreN board target = s
+    (filter (\domino -> goesP board domino L) hand, -- Left end
+    filter (\domino -> goesP board domino R) hand)  -- Right end  
+  
+  goesP && not(playedP ) && (scoreBoard == target)
+  
+  
+  
+  
+  
+  
+  
+  
+  
